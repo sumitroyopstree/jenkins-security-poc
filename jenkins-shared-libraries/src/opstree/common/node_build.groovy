61a78a229d20684fafd02af46e5f885658e31837
Priyanshu Yadav <priyanshu.yadav@opstree.com>
2026-09-01 23:43:45 +0530

    perf(node): add --prefer-offline flag for yarn and npm to speedup installs and leverage host cache

--- jenkins-shared-libraries/src/opstree/common/node_build.groovy
+++ jenkins-shared-libraries/src/opstree/common/node_build.groovy
@@ -2077,13 +2077,13 @@ def build_and_package_node(Map step_params) {
     def prune_command = ''
 
     if (package_manager == 'yarn') {
-        install_command = 'yarn install'
+        install_command = 'yarn install --prefer-offline'
         if (prune_dev_dependencies) {
             // Corrected: --production=true retains dependencies and strips devDependencies
             prune_command = 'yarn install --production=true --ignore-scripts --prefer-offline'
         }
     } else {
-        install_command = 'npm install'
+        install_command = 'npm install --prefer-offline'
         if (prune_dev_dependencies) {
             prune_command = 'npm prune --omit=dev'
         }
