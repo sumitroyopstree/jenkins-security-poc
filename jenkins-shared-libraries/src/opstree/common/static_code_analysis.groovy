6d60299b13381f82f1e160b4e12baeb231c8a008
Priyanshu Yadav <priyanshu.yadav@opstree.com>
2026-09-02 12:52:50 +0530

    chore: update build scripts

--- jenkins-shared-libraries/src/opstree/common/static_code_analysis.groovy
+++ jenkins-shared-libraries/src/opstree/common/static_code_analysis.groovy
@@ -53,6 +53,7 @@ def sonar(Map step_params) {
                         "-Dsonar.projectName=${sonar_project_name} " +
                         "-Dsonar.sources=${actual_sources} " +
                         "-Dsonar.host.url=${actual_host} " +
+                        "-Dsonar.exclusions=**/node_modules/**,**/dist/**,**/.next/**,**/build/**,**/coverage/** " +
                         "-Dsonar.javascript.lcov.reportPaths=lcov.info,coverage/lcov.info " +
                         "-Dsonar.typescript.lcov.reportPaths=lcov.info,coverage/lcov.info " +
                         "-Dsonar.scm.exclusions.disabled=true " +
